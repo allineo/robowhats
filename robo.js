f2cb51d1a58441c31a562c9d43c46e8f2df429e1
Alline Oliveira <alline.oliveira@gmail.com>
2021-05-18 12:27:07 -0300

    Update robo.js

--- robo.js
+++ robo.js
@@ -1,10 +1,27 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// using the http module
+let http = require('http'),
+ 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
+ 
+// create a simple server
+let server = http.createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
 });
 
 
