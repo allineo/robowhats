11f21abab81a1072325b793ae0cbabad1ec83a62
Alline Oliveira <alline.oliveira@gmail.com>
2021-05-19 11:49:16 -0300

    Update robo.js

--- robo.js
+++ robo.js
@@ -1,28 +1,57 @@
-// using the http module
-let http = require('http'),
- 
-// look for PORT environment variable, 
-// else look for CLI argument,
-// else use hard coded value for port 8080
-port = process.env.PORT || process.argv[2] || 8080;
- 
-// create a simple server
-let server = http.createServer(function (req, res) {
- 
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.write('hello heroku!', 'utf-8');
-        res.end();
- 
+const wppconnect = require('@wppconnect-team/wppconnect');
+
+var userStages = [];
+
+wppconnect.create()
+    .then((client) => start(client))
+    .catch((error) => console.log(error));
+
+
+function start(client) {
+    client.onMessage((message) => {
+        console.log('Mensagem digitada pelo usuário: ' + message.body);
+        stages(client, message);
     });
- 
-// listen on the port
-server.listen(port, function () {
- 
-    console.log('app up on port: ' + port);
- 
-});
+}
+
+//  Stages = Olá  >>  Nome  >>  CPF  >> Fim
+function stages(client, message) {
+    stage = userStages[message.from];
+    switch (stage) {
+        case 'Nome':
+            const nome = message.body;
+            sendWppMessage(client, message.from, 'Obrigada, ' + nome);
+            sendWppMessage(client, message.from, 'Digite seu *CPF*:');
+            userStages[message.from] = 'CPF';
+            break;
+        case 'CPF':
+            const cpf = message.body;
+            sendWppMessage(client, message.from, 'Obrigada por informar seu CPF: ' + cpf);
+            sendWppMessage(client, message.from, 'Fim');
+            userStages[message.from] = 'Fim';
+            break;
+        case 'Fim':
+            sendWppMessage(client, message.from, 'Fim');
+            break;
+        default: // Olá 
+            console.log('*Usuário atual* from:' + message.from + ' pushname:' + message.sender.pushname);
+            sendWppMessage(client, message.from, 'Bem vindo ao Robô de Whatsapp do AppBasicão!');
+            sendWppMessage(client, message.from, 'Digite seu *NOME*:');
+            userStages[message.from] = 'Nome';
+    }
+}
+
+
+function sendWppMessage(client, sendTo, text) {
+    client
+        .sendText(sendTo, text)
+        .then((result) => {
+            // console.log('SUCESSO: ', result); 
+        })
+        .catch((erro) => {
+            console.error('ERRO: ', erro);
+        });
+}
 
 
 
