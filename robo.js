f24a637ff4c218e55c4280ac2d8249b7929ce36d
Alline Oliveira allineo <alline.oliveira@gmail.com>
2021-06-04 13:35:46 -0300

    fixes

--- robo.js
+++ robo.js
@@ -1,94 +1,112 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
+const firebaseadmin = require('firebase-admin');
 
 
-let http = require('http'),
-    port = process.env.PORT || process.argv[2] || 8080;
-
+const firebaseServiceAccount = require('./robowhats-a9c82-firebase-adminsdk-cwr9k-634467143f.json');
+firebaseadmin.initializeApp({
+    credential: firebaseadmin.credential.cert(firebaseServiceAccount)
+});
+const db = firebaseadmin.firestore();
 
 var userStages = [];
 
-
-let server = http.createServer(function (req, res) {
-
-    const myTokenStore = new wppconnect.tokenStore.FileTokenStore({
-        decodeFunction: JSON.parse,
-        encodeFunction: JSON.stringify,
-        encoding: 'utf8',
-        fileExtension: '.json',
-        path: './tokens',
-    });
-
-    wppconnect.create({
-        session: 'test',
-        tokenStore: myTokenStore,
-        catchQR: (base64Qrimg, asciiQR, attempts, urlCode) => {
-
-        },
-        statusFind: (statusSession, session) => {
-            console.log('Status Session: ', statusSession, '\n');
-            console.log('Session name: ', session, '\n');
-        },
-        folderNameToken: 'tokens',
-        headless: true,
-        devtools: false,
-        useChrome: true,
-        debug: false,
-        logQR: true,
-        browserArgs: [
-            '--log-level=3',
-            '--no-default-browser-check',
-            '--disable-site-isolation-trials',
-            '--no-experiments',
-            '--ignore-gpu-blacklist',
-            '--ignore-certificate-errors',
-            '--ignore-certificate-errors-spki-list',
-            '--disable-gpu',
-            '--disable-extensions',
-            '--disable-default-apps',
-            '--enable-features=NetworkService',
-            '--disable-setuid-sandbox',
-            '--no-sandbox',
-            '--disable-webgl',
-            '--disable-threaded-animation',
-            '--disable-threaded-scrolling',
-            '--disable-in-process-stack-traces',
-            '--disable-histogram-customizer',
-            '--disable-gl-extensions',
-            '--disable-composited-antialiasing',
-            '--disable-canvas-aa',
-            '--disable-3d-apis',
-            '--disable-accelerated-2d-canvas',
-            '--disable-accelerated-jpeg-decoding',
-            '--disable-accelerated-mjpeg-decode',
-            '--disable-app-list-dismiss-on-blur',
-            '--disable-accelerated-video-decode',
-        ],
-        disableSpins: true,
-        disableWelcome: true,
-        updatesLog: true,
-        autoClose: 60000,
-        waitForLogin: true,
-        createPathFileToken: true,
-    })
-        .then((client) => start(client))
-        .catch((error) => console.log(error));
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello robowahts on Heroku!', 'utf-8');
-    res.end();
-});
-
-server.listen(port, function () {
-    console.log('app up on port: ' + port);
-});
-
+wppconnect.create()
+    .then((client) => start(client))
+    .catch((error) => console.log(error));
 
 
 function start(client) {
     client.onMessage((message) => {
+        console.log('*Usuário atual* from:' + message.from); 
         console.log('Mensagem digitada pelo usuário: ' + message.body);
-        //stages(client, message);
+        queryUserByPhone(client, message);
     });
 }
 
+async function queryUserByPhone(client, message) {
+    let phone = (message.from).replace(/[^\d]+/g, '');
+    let userid = null, userdata = null;
+    const queryRef = await db.collection('usuarios')
+        .where('whatsapp', '==', phone)
+        .get();
+    if (queryRef.empty) {
+        saveUser(client, message);
+    } else {
+        queryRef.forEach((user) => {
+            userid = user.id;
+            userdata = user.data();
+            console.log('Usuário corrente: ' + userid);
+            stages(client, message, userid, userdata);
+        });
+    }
+}
 
+
+//  Stages = ola  >>  nome  >>  cpf  >>  fim
+async function stages(client, message, userid, userdata) {
+    if (userStages[message.from] == undefined) {
+        sendWppMessage(client, message.from, 'Bem vindo ao Robô de Whatsapp do AppBasicão!');
+    }
+    if (userdata['nome'] == undefined) {
+        if (userStages[message.from] == undefined) {
+            sendWppMessage(client, message.from, 'Digite seu *NOME*:');
+            userStages[message.from] = 'nome';
+        } else {
+            userdata['nome'] = message.body;
+            updateUser(userid, userdata);
+            sendWppMessage(client, message.from, 'Obrigada, ' + message.body);
+            sendWppMessage(client, message.from, 'Digite seu *CPF*:');
+            userStages[message.from] = 'cpf';
+        }
+
+    } else if (userdata['cpf'] == undefined) {
+        if (userStages[message.from] == undefined) {
+            sendWppMessage(client, message.from, 'Digite seu *CPF*:');
+            userStages[message.from] = 'cpf';
+        } else {
+            userdata['cpf'] = (message.body).replace(/[^\d]+/g, '');
+            updateUser(userid, userdata);
+            sendWppMessage(client, message.from, 'Obrigada por informar seu CPF: ' + message.body);
+            sendWppMessage(client, message.from, 'Fim');
+            userStages[message.from] = 'fim';
+        }
+
+    } else {
+        if (userStages[message.from] == undefined) {
+            userStages[message.from] = 'fim';
+        }
+        sendWppMessage(client, message.from, 'Fim');
+    }
+}
+
+
+function sendWppMessage(client, sendTo, text) {
+    client.sendText(sendTo, text)
+        .then((result) => {
+            // console.log('SUCESSO: ', result); 
+        })
+        .catch((erro) => {
+            console.error('ERRO: ', erro);
+        });
+}
+
+async function saveUser(client, message) {
+    let pushname = '';
+    if (message['sender']['pushname'] != undefined) {
+        pushname = message['sender']['pushname'];
+    }
+    let user = {
+        'date': firebaseadmin.firestore.Timestamp.fromDate(new Date()),
+        'pushname': pushname,
+        'whatsapp': (message.from).replace(/[^\d]+/g, '')
+    }
+    const newUserRegister = await db.collection('usuarios').add(user);
+    console.log('Novo usuário cadastrado: ' + newUserRegister.id);
+    stages(client, message, newUserRegister.id, user);
+    return newUserRegister.id;
+}
+
+async function updateUser(userid, userdata) {
+    const userRegister = await db.collection('usuarios').doc(userid).set(userdata);
+    return userRegister;
+}
