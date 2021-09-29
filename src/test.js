6ec3b010d6f944e17ddd598f8aeb38f6f658c060
Alline Oliveira <alline.oliveira@gmail.com>
2021-09-29 13:43:25 -0300

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,19 +1,48 @@
-const { Client } = require('whatsapp-web.js');
-// const qrCode = require('qrcode-terminal');
 
 // https://github.com/pedroslopez/whatsapp-web.js
 
+
+const { Client } = require('whatsapp-web.js');
+//const qrCode = require('qrcode-terminal');
+const fs = require('fs');
+
+
+const SESSION_FILE_PATH = './session.json';
+
+let sessionData;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+
 //const client = new Client({ puppeteer: { headless: false }, session: sessionCfg });
 
-const client = new Client();
+//const client = new Client();
 /*{
    // session: 'whatswebtest',
     qrTimeoutMs: 0
     //puppeteer: { headless: false }
 });*/
 
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    },
+    qrTimeoutMs: 0,
+    session: sessionData
+});
+
+client.on('authenticated', (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
 client.on('qr', (qr) => {
-   // qrCode.generate(qr, {small: true});
+    // qrCode.generate(qr, {small: true});
     console.log('', qr);
 });
 
@@ -22,9 +51,12 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.body == '!ping') {
+    if (msg.body.toLowerCase() == '!ping') {
         msg.reply('pong');
     }
 });
 
 client.initialize();
+
+
+
